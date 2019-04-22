a3d7c44926ec60a07636204f5b26eca7b36bbcdf
Aimee <37088502+Aimzj@users.noreply.github.com>
2019-04-22 15:56:01 +0200

    Initialisation
    Basic project structure

---
+++ src/main/java/model/Board.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Board {
+
+    Integer[][] Cells;
+
+    final Integer X_SIZE = 16;
+    final Integer Y_SIZE = 16;
+    final Float BOMB_CHANCE = ;
+
+    public Board(){
+        Cells = new Integer[X_SIZE][Y_SIZE];
+
+        //initialise grid with bombs placed randomly
+
+    }
+}
---
+++ src/main/java/model/CellStatus.java
@@ -0,0 +1,14 @@
+package model;
+
+enum CellStatus {
+    BOMB,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    EMPTY
+}
