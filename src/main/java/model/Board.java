577e3edf65dd1485bf8e7e4ec2c81e529f96eef2
Aimzj <aimeejhandley@gmail.com>
2019-04-22 16:23:58 +0200

    Added more game logic

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,17 +1,55 @@
 package model;
 
+import java.util.Random;
+
 public class Board {
 
-    Integer[][] Cells;
+    CellStatus[][] Cells;
 
     final Integer X_SIZE = 16;
     final Integer Y_SIZE = 16;
-    final Float BOMB_CHANCE = ;
+    final Integer BOMB_CHANCE = 16;
 
     public Board(){
-        Cells = new Integer[X_SIZE][Y_SIZE];
+        Cells = new CellStatus[X_SIZE][Y_SIZE];
 
         //initialise grid with bombs placed randomly
+        for(Integer x=0; x<X_SIZE; x++){
+            for(Integer y=0; y<Y_SIZE; y++ ){
+                Random rand = new Random();
+                int cell = rand.nextInt(BOMB_CHANCE);
+                if(cell == 1){
+                    Cells[x][y]=CellStatus.BOMB;
+                }
+            }
+        }
+
+        //move through grid checking for number of bombs surrounding each cell
+        //set the cell status accordingly
+        Integer bombCount;
+        for(Integer x=0; x<X_SIZE; x++){
+            for(Integer y=0; y<Y_SIZE; y++ ){
+                //reset bomb count
+                bombCount=0;
+
+                //check above
+
+                //check below
+
+                //check left
+
+                //check right
+
+                //check upper left
+
+                //check upper right
+
+                //check lower left
+
+                //check lower right
+            }
+        }
+
 
     }
 }
