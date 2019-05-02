62e6e506d255a2e9c143c45c0004fd5ddf0557fd
Aimzj <aimeejhandley@gmail.com>
2019-05-02 13:37:37 +0200

    Added 2 unit tests

--- src/test/java/unitTests.java
+++ src/test/java/unitTests.java
@@ -1,19 +1,40 @@
 import org.junit.Test;
-import static junit.framework.TestCase.assertTrue;
+import za.co.bbd.model.Board;
+import za.co.bbd.model.Cell;
+
+import static junit.framework.TestCase.*;
 
 public class unitTests {
 
     @Test
-    public void someTest(){
-        //TODO: make unit test
+    public void BoardNotNullTest(){
+        Board board = new Board();
 
-        assertTrue(1==1);
+        for(int row=0; row<board.Y_SIZE; row++){
+            for(int column=0; column<board.X_SIZE; column++){
+                assertNotNull(board.Cells[row][column]);
+            }
+        }
     }
 
     @Test
-    public void otherTest(){
-        //TODO: make unit test
+    public void GameEndsOnBombClickTest(){
+        Board board = new Board();
 
-        assertTrue(1==1);
+        //loop through the board until a bomb is found
+        for(int row=0; row<board.Y_SIZE; row++){
+            for(int column=0; column<board.X_SIZE; column++){
+
+                board.openSpace(column, row);
+
+                if(!board.Cells[row][column].isClicked){
+                    if(board.Cells[row][column].getValue() == -1){
+                        assertTrue(board.isEndGame);
+                    }else {
+                        assertFalse(board.isEndGame);
+                    }
+                }
+            }
+        }
     }
 }
